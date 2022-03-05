988884269674fd82ffd78c347ef4835002dc3d41
Ivan <iskolyar@gmail.com>
2022-03-05 23:08:03 +0200

    changed field class to have 2d array as field

--- Snake/Field.cpp
+++ Snake/Field.cpp
@@ -1,35 +1,47 @@
 #include "Field.h"
+#include "Fruit.h"
 
-Field::Field(std::ostream& o, int h, int w, char c) : out(o), height(h), width(w), border_symbol(c)
+Field::Field(std::ostream& o, int h, int w, char border) : out(o), height(h), width(w), border_symbol(border)
 {
-
+	field.resize(height);
+	for (auto& row : field)
+		row.resize(width, ' ');
+	set_horizontal_borders();
+	set_vertical_borders();
 }
 
 void Field::print()
 {
-	print_horizontal_edge_border();
-
-	out << "\n";
-
-	for (int j = 0; j < height - 2; ++j)
+	for (int j = 0; j < height; ++j)
 	{
 		for (int i = 0; i < width; ++i)
 		{
-			if (i == 0 || i == width - 1)
-				out << border_symbol;
-			else
-				out << " ";
+			out << field[i][j];
 		}
 		out << "\n";
 	}
-
-	print_horizontal_edge_border();
 }
 
-void Field::print_horizontal_edge_border()
+void Field::set_fruit(Fruit& fruit)
+{
+	fruit.calculate_coordinates(width - 1, height - 1);
+	field[fruit.get_x()][fruit.get_y()] = fruit.get_symbol();
+}
+
+void Field::set_horizontal_borders()
 {
 	for (int i = 0; i < width; ++i)
 	{
-		out << border_symbol;
+		field[i][0] = '#';
+		field[i][height - 1] = '#';
+	}
+}
+
+void Field::set_vertical_borders()
+{
+	for (int j = 0; j < height; ++j)
+	{
+		field[0][j] = '#';
+		field[width - 1][j] = '#';
 	}
 }
--- Snake/Field.h
+++ Snake/Field.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <iostream>
+#include <vector>
+
+class Fruit;
 
 class Field
 {
@@ -8,15 +11,18 @@ private:
 	int height;
 	char border_symbol;
 
+	std::vector<std::vector<char>> field;
+
 	std::ostream &out;
 
 public:
 
-	Field(std::ostream& o, int h = 20, int w = 20, char c = '#');
+	Field(std::ostream& o, int h = 20, int w = 20, char border = '#');
 
 	void print();
-
+	void set_fruit(Fruit &fruit);
 private:
-	void print_horizontal_edge_border();
+	void set_horizontal_borders();
+	void set_vertical_borders();
 };
 
