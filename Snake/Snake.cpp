472165910dbe61f328cc58bc86d413063f77fef0
Ivan <iskolyar@gmail.com>
2022-03-06 11:16:27 +0200

    added user input processing

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -1,4 +1,5 @@
 #include "Snake.h"
+#include <conio.h>
 
 Snake::Snake()
 {
@@ -6,9 +7,47 @@ Snake::Snake()
 	body[0].symbol = 'O';
 }
 
+void Snake::set_initial_head_position(int i, int j)
+{
+	body[0].x = i;
+	body[0].y = j;
+}
+
 void Snake::add_link()
 {
 	Link tail = body[body.size() - 1];
 	Link new_link(tail.x, tail.y, 'o');
 	body.push_back(new_link);
+}
+
+void Snake::process_input()
+{
+	if (_kbhit())
+	{
+		switch (_getch())
+		{
+		case 'a':
+			move_left();
+			break;
+		case 'd':
+			move_right();
+			break;
+		case 'w':
+			move_top();
+			break;
+		case 's':
+			move_bottom();
+			break;
+		}
+		update_tail_position();
+	}
+}
+
+void Snake::update_tail_position()
+{
+	for (std::size_t i = 1; i < body.size(); ++i)
+	{
+		body[i].x = body[i - 1].x;
+		body[i].y = body[i - 1].y;
+	}
 }
--- Snake/Snake.h
+++ Snake/Snake.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <conio.h>
 
 struct Link {
 	Link(int i = 1, int j = 1, char c = 'o') : x(i), y(j), symbol(c) {}
@@ -17,10 +18,25 @@ private:
 public:
 	Snake();
 
+	void process_input();
+	void set_initial_head_position(int i, int j);
 	void add_link();
 	const std::vector<Link>& get_body() const { return body; }
 	
 	int get_head_x() const { return body[0].x; }
 	int get_head_y() const { return body[0].y; }
+
+private:
+	void move_left() { --body[0].x; }
+	void move_right() { ++body[0].x; }
+	
+	//move top uses decrement operator since field (0; 0)-point is located in top left corner 
+	//and to move top one needs to decrement coordinate
+	//the same with move bottom.
+	void move_top() { --body[0].y; } 
+	void move_bottom() { ++body[0].y; }
+
+	void update_tail_position();
+
 };
 
