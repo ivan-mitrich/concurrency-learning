2b2c8d4ef1ef699ce1d251be8c18d7c0b2be539e
Ivan <iskolyar@gmail.com>
2022-03-10 17:05:29 +0200

    added road length settings parameter and conditions due to which obstacle with greater speed will not catch obstacle with lower speed

--- Racing/ObstaclesSpawner.cpp
+++ Racing/ObstaclesSpawner.cpp
@@ -6,25 +6,21 @@ ObstaclesSpawner::ObstaclesSpawner(int minimum_x, int maximum_x)
 {
 	const int APPROXIMATE_OBSTACLES_AMOUNT = 10;
 	obstacles.reserve(APPROXIMATE_OBSTACLES_AMOUNT);
-
-
-
 }
 
 void ObstaclesSpawner::spawn_obstacle()
 {
-	int is_spawning_allowed = get_random_int_in_range(0, 1);
-	if (is_spawning_allowed) 
+	int is_spawning_allowed = get_random_int_in_range(0, 3);
+	if (is_spawning_allowed != 0) 
 	{
 		RoadObject* obstacle = nullptr;
-		const int NEW_OBSTACLE_WIDTH = 4;
 		
 		int obstacle_size = get_random_int_in_range(settings.min_size, settings.max_size);
 		int x_position = get_random_int_in_range(settings.min_x, settings.max_x - obstacle_size);
-		
-		if (is_x_position_possible(x_position, NEW_OBSTACLE_WIDTH))
-		{
-			int obstacle_speed = get_random_int_in_range(settings.min_speed, settings.max_speed);
+		int obstacle_speed = get_random_int_in_range(settings.min_speed, settings.max_speed);
+
+		if (is_x_position_possible(x_position, obstacle_size, obstacle_speed))
+		{			
 			char symbol = get_obstacle_symbol();
 			obstacle = new RoadObject(obstacle_size, obstacle_size, obstacle_speed, symbol);
 			obstacle->set_x_position(x_position);
@@ -37,6 +33,11 @@ void ObstaclesSpawner::set_spawning_x_range(int min, int max)
 {
 	settings.min_x = min;
 	settings.max_x = max;
+}
+
+void ObstaclesSpawner::set_road_length(int length)
+{
+	settings.road_length = length;
 }
 
 void ObstaclesSpawner::set_obstacles_size_range(int min, int max)
@@ -78,31 +79,45 @@ char ObstaclesSpawner::get_obstacle_symbol()
 	return settings.symbols[random_index];
 }
 
-bool ObstaclesSpawner::is_x_position_possible(int new_object_x, int new_object_width)
+bool ObstaclesSpawner::is_x_position_possible(int new_object_x, int new_object_size, int new_obstacle_speed)
 {
 	for (const auto& obstacle : obstacles)
 	{
-		if (obstacle->get_y_position() <= 0)
+		int obstacle_y_coordinate = obstacle->get_y_position();
+		if (obstacle_y_coordinate <= 0)
 		{
 			return false;
 		}
-		else if (obstacle->get_y_position() > obstacle->get_height())
+		else 
 		{
-			continue;
+			float obstacles_speed_ratio = (float)new_obstacle_speed / (float)obstacle->get_speed();
+			
+			if( std::round(obstacles_speed_ratio) != 1.f )
+			{ 			
+				int y_distance = std::ceil(obstacles_speed_ratio * (settings.road_length - obstacle_y_coordinate));
+				if (obstacle->get_y_position() > y_distance)
+				{
+					continue;
+				}
+			}
+			else if (obstacle->get_y_position() > obstacle->get_height())
+			{
+				continue;
+			}
 		}
 
 
-		int distance = 0;
+		int x_distance = 0;
 		if (new_object_x < obstacle->get_x_position())
 		{
-			distance = std::abs(obstacle->get_x_position() + obstacle->get_width() - new_object_x);
+			x_distance = std::abs(obstacle->get_x_position() + obstacle->get_width() - new_object_x);
 		}
 		else
 		{
-			distance = std::abs(new_object_x + new_object_width - obstacle->get_x_position());
+			x_distance = std::abs(new_object_x + new_object_size - obstacle->get_x_position());
 		}
 
-		if (distance < new_object_width + obstacle->get_width())
+		if (x_distance < new_object_size + obstacle->get_width())
 		{
 			return false;
 		}		
--- Racing/ObstaclesSpawner.h
+++ Racing/ObstaclesSpawner.h
@@ -5,6 +5,8 @@ struct ObstacleSpawnerSettings {
 	int min_x = 0;
 	int max_x = 1;
 
+	int road_length = 1;
+
 	int min_speed = 1;
 	int max_speed = 1;
 
@@ -30,12 +32,13 @@ public:
 	const std::vector<RoadObject*>& get_obstacles() const { return obstacles; }
 
 	void set_spawning_x_range(int min, int max);
+	void set_road_length(int length);
 	void set_obstacles_size_range(int min, int max);
 	void set_obstacles_speed_range(int min, int max);
 	void set_obstacles_symbols(const std::vector<char>& symbols);
 private:
 	static int get_random_int_in_range(int min, int max);
 	char get_obstacle_symbol();
-	bool is_x_position_possible(int new_object_x, int new_object_width);
+	bool is_x_position_possible(int new_object_x, int new_object_width, int new_obstacle_speed);
 };
 
