3484855068e44261fe9d8133c4275194464d51d3
Ivan <iskolyar@gmail.com>
2022-03-11 19:15:25 +0200

    added comments

--- Racing/Game.h
+++ Racing/Game.h
@@ -4,36 +4,70 @@
 #include "ObstaclesSpawner.h"
 #include "Timer.h"
 
+/*
+* Main class to handle gameplay events such as player input, objects moving, collisions ...
+*/
+
 class Game
 {
 private:
+	/* Represents playable area as 2D char array */
 	Field field;
+
+	/* Represents car controlled by player */
 	Car player_car;
+
+	/* Represents road obstacles spawner class */
 	ObstaclesSpawner spawner;
+
+	/* Represents wrapper of std::chrono::high_resolution_clock needed to record player time in the game */
 	Timer timer;
 
+	/* Represents condition if game is paused */
 	bool paused;
+
+	/* Represents condition if game is over */
 	bool game_over;
 
 public:
 	Game();
+
+	/* Called to launch game */
 	void start_game();
-	void display_help();
 
 private:
-	void redraw_screen();
-	void update_field();
-	void check_collisions();
 
+	/* Called before game is started to print info how to drive the car with keyboard */
+	void display_help();
+	
+	void move_obstacles();
+	void check_collisions();
+	void redraw_screen();
+
+
+	void draw_obstacles();
+	void update_field();
+	
+	/* Called in check collisions function for each obstacle to check if it intersect the player car */
 	void check_car_obstacle_collision(const RoadObject const *object);
+
+	/* 
+	*  Needed to calculate intersection between two objects in specific dimension (x or y) 
+	*  object1_min - left or top field-coordinate of object1 depending on chosen dimension (x or y) where intersection are finding
+	*  object1_max - object1_min + object1 size in chosen dimension
+	*  object2_min - left or top field-coordinate of object2 depending on chosen dimension (x or y) where intersection are finding
+	*  object2_min - object2_min + object2 size in chosen dimension
+	*/
 	bool has_dimension_intersection_between_objects(int object1_min, int object1_max, int object2_min, int object2_max, int sum_leght);
 
+	/* Print played time when game is over */
 	void print_score();
 
+	/* Handle user input when he/she press the keyboard */
 	void process_user_input();
+	
+	/* Function to launch process_user_input() in while loop in separate thread */
 	void user_input_loop();
 
-	void move_obstacles();
-	void draw_obstacles();
 };
 
