33f796fef0eaf5701d211572c2462c02d4dcce8c
Ivan <iskolyar@gmail.com>
2022-03-23 18:54:02 +0200

    jklsdfasdfhk;asasf;agshdfa

--- File-RW-Test/simple_mutex_producer_consumer.cpp
+++ File-RW-Test/simple_mutex_producer_consumer.cpp
@@ -1,25 +1,31 @@
 #include "simple_mutex_producer_consumer.h"
 #include <iostream>
 #include <random>
+#include <condition_variable>
 
-const int simple_mutex::LOOP_COUNTER = 1000;
+const int simple_mutex::LOOP_COUNTER = 13;
 const int simple_mutex::NOT_A_VALID_DATA = -111;
 bool simple_mutex::is_produced = false;
 bool simple_mutex::is_over = false;
 std::mutex simple_mutex::mutex;
 int simple_mutex::data_store = simple_mutex::NOT_A_VALID_DATA;
+std::condition_variable data_cond;
+bool can_produce = true;
 
 void simple_mutex::producer_thread_function() {
+
 	for (int i = 0; i < simple_mutex::LOOP_COUNTER; ++i)
 	{
-		
-		//std::this_thread::sleep_for(std::chrono::milliseconds(1));
-		
-		
-		simple_mutex::data_store = simple_mutex::produce_data();
-		simple_mutex::is_produced = true;
 		std::unique_lock<std::mutex> lock(simple_mutex::mutex);
+		//data_cond.wait(lock, [] { 
+			//std::cout << "Producer wait-function checking ..." << std::endl;
+		//	return !simple_mutex::is_produced; 
+		//	});
+		simple_mutex::data_store = produce_data();
+		simple_mutex::is_produced = true;
+		data_cond.notify_one();
 	}
+	
 	is_over = true;
 }
 
@@ -27,13 +33,16 @@ void simple_mutex::consumer_thread_function() {
 	while (true)
 	{	
 		std::unique_lock<std::mutex> lock(simple_mutex::mutex);
-		if (simple_mutex::is_produced)
-		{		
-			int data = data_store;
-			data_store = simple_mutex::NOT_A_VALID_DATA;
-			simple_mutex::is_produced = false;
-			consume_data(data);
-		}
+		data_cond.wait(lock, [] { 
+			//std::cout << "Consumer wait-function checking ..." << std::endl;
+			return simple_mutex::is_produced; 
+			});
+		int data = simple_mutex::data_store;
+		consume_data(data);
+		simple_mutex::is_produced = false;
+		
+		lock.unlock();
+		//data_cond.notify_one();
 		if (is_over)
 		{
 			break;
