441d4706db93b9fd1d7d5f8fc200901f5b50e77f
Ivan <iskolyar@gmail.com>
2022-03-23 20:20:50 +0200

    added fixed int buffer class

---
+++ File-RW-Test/FixedIntBuffer.cpp
@@ -0,0 +1,18 @@
+#include "FixedIntBuffer.h"
+
+FixedIntBuffer::FixedIntBuffer(std::size_t size_of_buffer)
+{
+
+}
+
+int FixedIntBuffer::pop()
+{
+	return 0;
+}
+
+void FixedIntBuffer::push(int value)
+{
+
+}
+
+
